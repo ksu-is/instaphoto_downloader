e66c622815584af606eb98ff137a3dc9035190f2
RIKA0731 <rmatano@students.kennesaw.edu>
2022-04-21 10:20:36 -0400

    Update instaphoto_downloader.py
    Added comments

--- Code/instaphoto_downloader.py
+++ Code/instaphoto_downloader.py
@@ -1,6 +1,7 @@
 import requests
 import re
 
+#Get the response for the request
 def get_response(url):
     r = requests.get(url)
     while r.status_code != 200:
@@ -10,15 +11,19 @@ def get_response(url):
 def prepare_urls(matches):
     return list({match.replace("\\u0026", "&") for match in matches})
 
+#Take the instagram user input
 url = input("Enter Instgram URL: ")
 response = get_response(url)
 
+#Match the pattern to get the URLs
 vid_matches = re.findall('"video_url":"([^"]+)"', response)
 pic_matches = re.findall('"display_url":"([^"]+)"', response)
+
 
 vid_urls = prepare_urls(vid_matches)
 pic_urls = prepare_urls(pic_matches)
 
+#Print a list of URLs
 if vid_urls:
     print('Detected Videos:\n{0}'.format('\n'.join(vid_urls)))
 
